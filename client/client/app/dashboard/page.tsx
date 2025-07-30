192de80c33770722f663bd9cbfe2bb6d234ffddf
Lukas Cec <lukinecka123987@gmail.com>
2025-07-30 22:21:43 +0200

    [HOME] Icons for sections

--- client/client/app/dashboard/page.tsx
+++ client/client/app/dashboard/page.tsx
@@ -15,7 +15,7 @@ export default function DashboardPage() {
     if (!user) return <div>Načítavam...</div>;
 
     return (
-        <div className="min-h-screen bg-white flex flex-col">
+        <div className="min-h-screen  flex flex-col">
 
             <section className="flex-1 flex flex-col justify-center items-center px-6 text-center">
                 <h1 className="text-4xl sm:text-6xl font-bold text-gray-900 mb-4">
@@ -33,26 +33,26 @@ export default function DashboardPage() {
             </section>
 
             {/* Výhody */}
-            <section className="py-16 px-6 bg-gray-50">
+            <section className="py-16 px-6 bg-white">
                 <div className="max-w-5xl mx-auto text-center">
                     <h2 className="text-3xl font-bold mb-12">Prečo DevMatch?</h2>
                     <div className="grid sm:grid-cols-3 gap-10 text-left">
-                        <div>
-                            <Image src="/icons/chat.svg" alt="Chat icon" width={40} height={40} />
+                        <div className="flex flex-col">
+                            <Image className="justify-center align-middle " src="/icons/chat.png" alt="Chat icon" width={140} height={140} />
                             <h3 className="text-xl font-semibold mt-4 mb-2">Chatuj v reálnom čase</h3>
                             <p className="text-gray-600 text-sm">
                                 Vďaka moderným technológiám si píšeš s ostatnými bez zbytočného čakania.
                             </p>
                         </div>
                         <div>
-                            <Image src="/icons/connect.svg" alt="Connect icon" width={40} height={40} />
+                            <Image src="/icons/connect.png" alt="Connect icon" width={140} height={140} />
                             <h3 className="text-xl font-semibold mt-4 mb-2">Spoj sa s devmi</h3>
                             <p className="text-gray-600 text-sm">
                                 Matchni sa na základe zručností a záujmov. Tvoji noví kolegovia čakajú.
                             </p>
                         </div>
                         <div>
-                            <Image src="/icons/profile.svg" alt="Profile icon" width={40} height={40} />
+                            <Image src="/icons/profile.png" alt="Profile icon" width={140} height={140} />
                             <h3 className="text-xl font-semibold mt-4 mb-2">Buduj svoj profil</h3>
                             <p className="text-gray-600 text-sm">
                                 Predstav sa a nechaj ostatných zistiť, kto si a na čom pracuješ.
